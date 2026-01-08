8e175c8cb75f3dbb70ab91b40819e815a2eef18c
arthursanchez2004 <arthursanchez2004@gmail.com>
2026-01-08 16:39:48 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Site VitaWeb carregado");
+const elements = document.querySelectorAll(".box, .card, .steps div");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+elements.forEach(el => observer.observe(el));
+
